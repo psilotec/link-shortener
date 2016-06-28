7bf3a22700658912d1ef9c194b2d1eb87518a16e
psilotec <scottadevito@gmail.com>
2016-06-27 21:26:48 -0400

    Created LinkCreate Component
    Styled with TWBS.

---
+++ client/components/link_create.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Scott on 6/27/2016.
+ */
+import React, { Component } from 'react';
+
+class LinkCreate extends Component {
+    render() {
+        return (
+          <form>
+              <div className="form-group">
+                  <label>Link to shorten</label>
+                  <input className="form-control" />
+              </div>
+              <button className="btn btn-primary">Shorten</button>
+          </form>
+        );
+    };
+};
+
+export default LinkCreate;
--- client/main.js
+++ client/main.js
@@ -3,12 +3,15 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Header from './components/header';
+import LinkCreate from './components/link_create';
 
 const App = () => {
   return (
     <div>
         <Header />
+        <LinkCreate />
     </div>
   );
 };
