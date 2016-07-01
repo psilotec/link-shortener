921ce334f93c021a772e4436f48751790d558995
psilotec <scottadevito@gmail.com>
2016-06-30 22:26:25 -0400

    Created Error Handling for Improper URL Submission

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -4,11 +4,22 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { error: '' };
+    }
+
     handleSubmit(event) {
         event.preventDefault();
 
         Meteor.call('links.insert', this.refs.link.value, (error) => {
-            console.log(error);
+            if (error) {
+                this.setState({error: 'Enter a valid URL'});
+            } else {
+                this.setState({ error: ''});
+                this.refs.link.value = '';
+            }
         });
     }
 
@@ -19,6 +30,7 @@ class LinkCreate extends Component {
                   <label>Link to shorten</label>
                   <input ref="link" className="form-control" />
               </div>
+              <div className="text-danger">{this.state.error}</div>
               <button className="btn btn-primary">Shorten</button>
           </form>
         );
