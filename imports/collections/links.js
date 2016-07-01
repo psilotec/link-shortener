79b0aacedc87a9a562086340a182694a871efd92
psilotec <scottadevito@gmail.com>
2016-06-30 22:14:28 -0400

    Added check to Ensure url is Valid
    Match.Where is used for when a custom validation is defined.  If
    Match.Where returns true then there is no error.

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -2,10 +2,13 @@
  * Created by Scott on 6/27/2016.
  */
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 Meteor.methods({
     'links.insert': function(url) {
         console.log('attempting to save', url);
+        check(url, Match.Where(url => validUrl.isUri(url)));
     }
 });
 
