68407c14675b33f2ad0a81ff2d6459647c4e892b
psilotec <scottadevito@gmail.com>
2016-07-21 23:21:49 -0400

    Added Middleware to Server
    Added console.log to debug new middleware function.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
 
 //Publish the whole Links collection
 Meteor.startup(() => {
@@ -7,3 +8,7 @@ Meteor.startup(() => {
         return Links.find({});
     })
 });
+
+//Middleware to check if incoming request has a token or not
+WebApp.connectHandlers
+    .use(req => console.log(req));
