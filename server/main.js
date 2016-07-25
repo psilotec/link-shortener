ec41673c0cd0bb431b9458b6b194d8995f95cb96
psilotec <scottadevito@gmail.com>
2016-07-24 22:21:25 -0400

    Created Helper Function to Redirect User
    With conditions to handle input possibilities.

--- server/main.js
+++ server/main.js
@@ -10,6 +10,20 @@ Meteor.startup(() => {
     })
 });
 
+//Execute whenever a user visits with a route like 'localhost:3000/abcd'
+function onRoute(req, res, next) {
+    //Take the token out of the URL and try to find a matching link in Links collection
+    const link = Links.findOne({ token: req.params.token });
+
+    //If the link object is found, redirect user to long URL
+    if (link) {
+        res.writeHead(307, { 'Location': link.url });
+        res.end();
+    } else {
+    //If link object isn't found, send user to React app
+    }
+}
+
 const middleware = ConnectRoute(function(router) {
    router.get('/:token', (req) => console.log(req));
 });
